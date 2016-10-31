a39df8269418b3b449260ff30c0c239a70ac4001
EdipoSouza <edipo2s@gmail.com>
2016-10-30 23:22:17 -0200

    Configure CardsFragment with tabs

---
+++ app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/cards/CardsCardsFragment.kt
@@ -0,0 +1,24 @@
+package com.ediposouza.teslesgendstracker.ui.cards
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ediposouza.teslesgendstracker.R
+import kotlinx.android.synthetic.main.activity_dash.*
+
+/**
+ * Created by EdipoSouza on 10/30/16.
+ */
+class CardsCardsFragment : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_cards_favorites, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity.dash_toolbar_title.setText(R.string.app_name)
+    }
+
+}
