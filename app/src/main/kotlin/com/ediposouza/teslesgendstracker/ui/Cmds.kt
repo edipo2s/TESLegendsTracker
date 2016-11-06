07eed0ff66a4a8ef5484de953ab37e35a5726d0a
EdipoSouza <edipo2s@gmail.com>
2016-11-06 12:57:29 -0200

    Add search filter

--- app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/Cmds.kt
+++ app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/Cmds.kt
@@ -5,6 +5,8 @@ import com.ediposouza.teslesgendstracker.data.CardRarity
 /**
  * Created by EdipoSouza on 11/5/16.
  */
+data class CmdFilterSearch(val search: String?)
+
 data class CmdFilterRarity(val rarity: CardRarity?)
 
 data class CmdFilterMagika(val magika: Int)
