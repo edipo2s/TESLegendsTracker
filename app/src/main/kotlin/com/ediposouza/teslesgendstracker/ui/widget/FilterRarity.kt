3bc4e374aad483560d474d45b2db56f12a6762a1
EdipoSouza <edipo2s@gmail.com>
2016-11-06 00:25:02 -0200

    Create RarityFilterWidget

---
+++ app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/widget/FilterRarity.kt
@@ -0,0 +1,104 @@
+package com.ediposouza.teslesgendstracker.ui.widget
+
+import android.animation.Animator
+import android.animation.ValueAnimator
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import com.ediposouza.teslesgendstracker.R
+import com.ediposouza.teslesgendstracker.data.CardRarity
+import kotlinx.android.synthetic.main.widget_rarity_filter.view.*
+
+/**
+ * Created by EdipoSouza on 11/2/16.
+ */
+class FilterRarity(ctx: Context?, attrs: AttributeSet?, defStyleAttr: Int) :
+        LinearLayout(ctx, attrs, defStyleAttr) {
+
+    val ANIM_DURATION: Long = 200
+    val rarityBGMinWidth by lazy { resources.getDimensionPixelSize(R.dimen.rarity_width) }
+    val rarityBGMaxWidth by lazy { resources.getDimensionPixelSize(R.dimen.rarity_bg_max_width) }
+
+    var filterClick: ((CardRarity) -> Unit)? = null
+
+    init {
+        inflate(context, R.layout.widget_rarity_filter, this)
+        if (!isInEditMode) {
+            with(rootView) {
+                rarity_filter_common.setOnClickListener { rarityClick(CardRarity.COMMON) }
+                rarity_filter_rare.setOnClickListener { rarityClick(CardRarity.RARE) }
+                rarity_filter_epic.setOnClickListener { rarityClick(CardRarity.EPIC) }
+                rarity_filter_legendary.setOnClickListener { rarityClick(CardRarity.LEGENDARY) }
+                rarity_filter_unique.setOnClickListener { rarityClick(CardRarity.UNIQUE) }
+                rarity_filter.setOnClickListener {
+                    if (rootView.rarity_filter_common?.visibility == View.GONE) {
+                        expand()
+                    } else {
+                        collapse()
+                    }
+                }
+            }
+        }
+    }
+
+    constructor(ctx: Context?) : this(ctx, null, 0) {
+    }
+
+    constructor(ctx: Context?, attrs: AttributeSet) : this(ctx, attrs, 0) {
+    }
+
+    private fun expand() {
+        with(rootView) {
+            with(ValueAnimator.ofInt(rarityBGMinWidth, rarityBGMaxWidth)) {
+                duration = ANIM_DURATION
+                addUpdateListener {
+                    rarity_filter_bg.layoutParams.width = it.animatedValue as Int
+                }
+                addListener(object : Animator.AnimatorListener {
+                    override fun onAnimationStart(p0: Animator?) {
+                    }
+
+                    override fun onAnimationRepeat(p0: Animator?) {
+                    }
+
+                    override fun onAnimationEnd(p0: Animator?) {
+                        rarity_filter_common?.visibility = View.VISIBLE
+                        rarity_filter_rare?.visibility = View.VISIBLE
+                        rarity_filter_epic?.visibility = View.VISIBLE
+                        rarity_filter_legendary?.visibility = View.VISIBLE
+                        rarity_filter_unique?.visibility = View.VISIBLE
+                    }
+
+                    override fun onAnimationCancel(p0: Animator?) {
+                    }
+                })
+                start()
+            }
+        }
+    }
+
+    private fun collapse() {
+        with(rootView) {
+            rarity_filter_common?.visibility = View.GONE
+            rarity_filter_rare?.visibility = View.GONE
+            rarity_filter_epic?.visibility = View.GONE
+            rarity_filter_legendary?.visibility = View.GONE
+            rarity_filter_unique?.visibility = View.GONE
+            with(ValueAnimator.ofInt(rarityBGMinWidth, rarityBGMaxWidth)) {
+                reverse()
+                duration = ANIM_DURATION
+                addUpdateListener {
+                    rarity_filter_bg.layoutParams.width = it.animatedValue as Int
+                }
+                start()
+            }
+        }
+    }
+
+    private fun rarityClick(rarity: CardRarity) {
+        filterClick?.invoke(rarity)
+        collapse()
+    }
+
+}
