d1ee28cb40b5077f85f8495aafae7f7847546457
EdipoSouza <edipo2s@gmail.com>
2016-12-29 00:32:20 -0200

    Comment deck support (Add and Remove)

--- app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/DeckActivity.kt
+++ app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/DeckActivity.kt
@@ -3,28 +3,34 @@ package com.ediposouza.teslesgendstracker.ui
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.BottomSheetBehavior
 import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import com.bumptech.glide.Glide
-import com.ediposouza.teslesgendstracker.MetricScreen
-import com.ediposouza.teslesgendstracker.R
-import com.ediposouza.teslesgendstracker.TIME_PATTERN
+import com.ediposouza.teslesgendstracker.*
 import com.ediposouza.teslesgendstracker.data.Deck
+import com.ediposouza.teslesgendstracker.data.DeckComment
 import com.ediposouza.teslesgendstracker.interactor.PrivateInteractor
 import com.ediposouza.teslesgendstracker.interactor.PublicInteractor
 import com.ediposouza.teslesgendstracker.manager.MetricsManager
-import com.ediposouza.teslesgendstracker.toogleExpanded
 import com.ediposouza.teslesgendstracker.ui.base.BaseActivity
 import com.ediposouza.teslesgendstracker.ui.utils.CircleTransform
+import com.google.firebase.auth.FirebaseAuth
+import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator
 import kotlinx.android.synthetic.main.activity_deck.*
 import kotlinx.android.synthetic.main.include_deck_info.*
+import kotlinx.android.synthetic.main.itemlist_deck_comment.view.*
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.intentFor
@@ -32,8 +38,9 @@ import org.jetbrains.anko.toast
 import org.threeten.bp.format.DateTimeFormatter
 import timber.log.Timber
 import java.text.NumberFormat
+import java.util.*
 
-class DeckActivity() : BaseActivity() {
+class DeckActivity : BaseActivity() {
 
     companion object {
 
@@ -49,10 +56,12 @@ class DeckActivity() : BaseActivity() {
 
     }
 
+    val publicInteractor by lazy { PublicInteractor() }
+    val privateInteractor by lazy { PrivateInteractor() }
+    val numberInstance by lazy { NumberFormat.getNumberInstance() }
     val deck by lazy { intent.getParcelableExtra<Deck>(EXTRA_DECK) }
     val deckOwned by lazy { intent.getBooleanExtra(EXTRA_OWNED, false) }
-    val numberInstance by lazy { NumberFormat.getNumberInstance() }
-    val privateInteractor by lazy { PrivateInteractor() }
+    val commentsSheetBehavior by lazy { BottomSheetBehavior.from(deck_bottom_sheet) }
 
     var favorite: Boolean = false
     var like: Boolean = false
@@ -69,7 +78,6 @@ class DeckActivity() : BaseActivity() {
 
         favorite = intent.getBooleanExtra(EXTRA_FAVORITE, false)
         like = intent.getBooleanExtra(EXTRA_LIKE, false)
-        val sheetBehavior = BottomSheetBehavior.from(deck_bottom_sheet)
         deck_fab_favorite.setOnClickListener {
             privateInteractor.setUserDeckFavorite(deck, !favorite) {
                 favorite = !favorite
@@ -77,7 +85,7 @@ class DeckActivity() : BaseActivity() {
                 setResult(Activity.RESULT_OK, Intent())
             }
         }
-        deck_bottom_sheet.setOnClickListener { sheetBehavior.toogleExpanded() }
+        deck_bottom_sheet.setOnClickListener { commentsSheetBehavior.toogleExpanded() }
         updateFavoriteItem()
         loadDeckInfo()
         setResult(Activity.RESULT_CANCELED, Intent())
@@ -86,15 +94,36 @@ class DeckActivity() : BaseActivity() {
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        deck_comment_send?.setOnClickListener {
+            if (deck_comment_new.text.toString().length < 4) {
+                alert(getString(R.string.deck_comment_size_error)) {
+                    okButton { }
+                    setTheme(R.style.AppDialog)
+                }.show()
+            } else {
+                PrivateInteractor().addDeckComment(deck, deck_comment_new.text.toString()) {
+                    deck_comment_new.setText("")
+                    addComment(it)
+                }
+            }
+        }
+        onKeyboardVisibilityChange = {
+            deck_comment_recycle_view.requestLayout()
+        }
+        if (!App.hasUserLogged) {
+            deck_comment_new?.isEnabled = false
+            deck_comment_send?.isEnabled = false
+            deck_comment_new?.hint = getText(R.string.deck_comment_new_hint_anonymous)
+        }
         doAsync {
             calculateMissingSoul(deck, privateInteractor)
-            PublicInteractor().getPatches {
+            publicInteractor.getPatches {
                 val patch = it.find { it.uidDate == deck.patch }
                 runOnUiThread {
                     deck_details_patch.text = patch?.desc ?: ""
                 }
             }
-            PublicInteractor().getUserInfo(deck.owner) {
+            publicInteractor.getUserInfo(deck.owner) {
                 val ownerUser = it
                 runOnUiThread {
                     deck_details_create_by.text = ownerUser.name
@@ -106,6 +135,14 @@ class DeckActivity() : BaseActivity() {
             }
         }
         MetricsManager.trackScreen(MetricScreen.SCREEN_DECK_DETAILS())
+    }
+
+    override fun onBackPressed() {
+        if (commentsSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+            commentsSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -167,23 +204,50 @@ class DeckActivity() : BaseActivity() {
         deck_class_cover.setImageResource(deck.cls.imageRes)
         deck_class_attr1.setImageResource(deck.cls.attr1.imageRes)
         deck_class_attr2.setImageResource(deck.cls.attr2.imageRes)
-        deck_comment_qtd.text = numberInstance.format(deck.comments.size)
         deck_details_type.text = deck.type.name.toLowerCase().capitalize()
         deck_details_views.text = numberInstance.format(deck.views)
         deck_details_likes.text = numberInstance.format(deck.likes.size)
         deck_details_soul_cost.text = numberInstance.format(deck.cost)
         deck_details_create_at.text = deck.createdAt.toLocalDate().toString()
+        val updateDate = deck.updatedAt.toLocalDate()
         val updateTime = deck.updatedAt.toLocalTime().format(DateTimeFormatter.ofPattern(TIME_PATTERN))
-        val updateText = "${deck.updatedAt.toLocalDate()} $updateTime"
-        deck_details_update_at.text = updateText
+        deck_details_update_at.text = getString(R.string.deck_details_last_update_format, updateDate, updateTime)
         deck_details_cardlist.showDeck(deck)
+        configDeckComments()
     }
 
-    fun calculateMissingSoul(deck: Deck, interactor: PrivateInteractor) {
+    private fun configDeckComments() {
+        with(deck_comment_recycle_view) {
+            adapter = DeckCommentAdapter(deck.comments, publicInteractor) {
+                privateInteractor.remDeckComment(deck, it) {
+                    remComment(it)
+                }
+            }
+            layoutManager = object : LinearLayoutManager(this@DeckActivity) {
+                override fun setMeasuredDimension(childrenBounds: Rect, wSpec: Int, hSpec: Int) {
+                    val maxHeight = resources.displayMetrics.heightPixels * getVisiblePercent()
+                    with(childrenBounds) {
+                        set(left, top, right, if (bottom < maxHeight) bottom else maxHeight.toInt())
+                    }
+                    super.setMeasuredDimension(childrenBounds, wSpec, hSpec)
+                }
+
+                private fun getVisiblePercent(): Float {
+                    Timber.d("Keyboard visible: " + keyboardVisible)
+                    return if (keyboardVisible) 0.2f else 0.6f
+                }
+            }
+            itemAnimator = SlideInLeftAnimator()
+            addItemDecoration(DividerItemDecoration(this@DeckActivity, DividerItemDecoration.VERTICAL))
+        }
+        deck_comment_qtd.text = numberInstance.format(deck.comments.size)
+    }
+
+    fun calculateMissingSoul(deck: Deck, privateInteractor: PrivateInteractor) {
         with(deck_details_soul_missing) {
             visibility = View.INVISIBLE
             deck_details_soul_missing_loading.visibility = View.VISIBLE
-            interactor.getMissingCards(deck, { deck_details_soul_missing_loading.visibility = View.VISIBLE }) {
+            privateInteractor.getMissingCards(deck, { deck_details_soul_missing_loading.visibility = View.VISIBLE }) {
                 deck_details_soul_missing_loading.visibility = View.GONE
                 val missingSoul = it.map { it.qtd * it.rarity.soulCost }.sum()
                 Timber.d("Missing %d", missingSoul)
@@ -192,6 +256,85 @@ class DeckActivity() : BaseActivity() {
                 deck_details_cardlist.showMissingCards(it)
             }
         }
+    }
+
+    private fun addComment(it: DeckComment) {
+        (deck_comment_recycle_view.adapter as DeckCommentAdapter).add(it)
+        deck_comment_recycle_view.scrollToPosition(0)
+        deck_comment_qtd.text = deck_comment_recycle_view.adapter.itemCount.toString()
+        deck_comment_recycle_view.requestLayout()
+        setResult(Activity.RESULT_OK, Intent())
+    }
+
+    private fun remComment(commentId: String) {
+        (deck_comment_recycle_view.adapter as DeckCommentAdapter).rem(commentId)
+        deck_comment_qtd.text = deck_comment_recycle_view.adapter.itemCount.toString()
+        deck_comment_recycle_view.requestLayout()
+        setResult(Activity.RESULT_OK, Intent())
+    }
+
+    class DeckCommentAdapter(val items: List<DeckComment>, val publicInteractor: PublicInteractor,
+                             val onRemComment: (commentId: String) -> Unit) : RecyclerView.Adapter<DeckCommentViewHolder>() {
+
+        init {
+            sortDeckComments()
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): DeckCommentViewHolder {
+            return DeckCommentViewHolder(parent?.inflate(R.layout.itemlist_deck_comment), onRemComment)
+        }
+
+        override fun onBindViewHolder(holder: DeckCommentViewHolder?, position: Int) {
+            holder?.bind(items[position], publicInteractor)
+        }
+
+        override fun getItemCount() = items.size
+
+        fun add(deckComment: DeckComment) {
+            (items as ArrayList).add(0, deckComment)
+            notifyItemInserted(0)
+        }
+
+        fun rem(commentId: String) {
+            val deckComment = items.find { it.id == commentId }
+            val deckCommentIndex = items.indexOf(deckComment)
+            (items as ArrayList).remove(deckComment)
+            notifyItemRemoved(deckCommentIndex)
+        }
+
+        private fun sortDeckComments() {
+            Collections.sort(items, { dc1, dc2 -> dc2.date.compareTo(dc1.date) })
+        }
+
+    }
+
+    class DeckCommentViewHolder(view: View?, val onRemComment: (commentId: String) -> Unit) : RecyclerView.ViewHolder(view) {
+
+        fun bind(comment: DeckComment, publicInteractor: PublicInteractor) {
+            val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+            itemView.deck_comment_msg.text = comment.comment
+            itemView.deck_comment_date.text = itemView.context.getString(R.string.deck_comment_date_format,
+                    comment.date.toLocalDate(), comment.date.toLocalTime().format(timeFormatter))
+            doAsync {
+                publicInteractor.getUserInfo(comment.owner) {
+                    val ownerUser = it
+                    itemView.post {
+                        itemView.deck_comment_owner.text = ownerUser.name
+                        with(itemView.deck_comment_delete) {
+                            val owner = comment.owner == FirebaseAuth.getInstance().currentUser?.uid
+                            visibility = if (owner) View.VISIBLE else View.GONE
+                            setOnClickListener { onRemComment(comment.id) }
+                        }
+                        Glide.with(itemView.context)
+                                .load(ownerUser.photoUrl)
+                                .fallback(R.drawable.ic_user)
+                                .transform(CircleTransform(itemView.context))
+                                .into(itemView.deck_comment_owner_photo)
+                    }
+                }
+            }
+        }
+
     }
 
 }
