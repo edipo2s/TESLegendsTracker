7e4194b054459e8cb5400c977b90dc7b400d6e73
EdipoSouza <edipo2s@gmail.com>
2017-01-03 00:37:47 -0200

    BaseFirebaseRVAdapter

--- app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/decks/tabs/DecksOwnerFragment.kt
+++ app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/decks/tabs/DecksOwnerFragment.kt
@@ -4,18 +4,15 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.*
-import android.widget.LinearLayout
 import android.widget.Switch
 import com.ediposouza.teslesgendstracker.R
 import com.ediposouza.teslesgendstracker.data.Class
 import com.ediposouza.teslesgendstracker.data.Deck
 import com.ediposouza.teslesgendstracker.interactor.FirebaseParsers
 import com.ediposouza.teslesgendstracker.interactor.PrivateInteractor
-import com.ediposouza.teslesgendstracker.ui.util.firebase.FirebaseRecyclerViewAdapter
+import com.ediposouza.teslesgendstracker.ui.base.BaseFirebaseRVAdapter
 import com.ediposouza.teslesgendstracker.util.inflate
-import com.google.firebase.database.DatabaseError
 import kotlinx.android.synthetic.main.fragment_decks_list.*
-import kotlinx.android.synthetic.main.itemlist_loading.view.*
 import timber.log.Timber
 
 /**
@@ -28,8 +25,21 @@ class DecksOwnerFragment : DecksPublicFragment() {
     private val privateInteractor = PrivateInteractor()
     private var onlyPrivate: Switch? = null
 
-    val firebaseDecksAdapter = OwnerFirebaseAdapter(itemClick, itemLongClick, privateInteractor, DECK_PAGE_SIZE) {
-        decks_refresh_layout.isRefreshing = false
+    val ownerDecksAdapter = object : BaseFirebaseRVAdapter<FirebaseParsers.DeckParser, DecksAllViewHolder>(
+            R.layout.itemlist_deck, FirebaseParsers.DeckParser::class.java, DecksAllViewHolder::class.java,
+            privateInteractor.getOwnedPrivateDecksRef(), DECK_PAGE_SIZE) {
+        override fun onCreateDefaultViewHolder(parent: ViewGroup): DecksAllViewHolder {
+            return DecksAllViewHolder(parent.inflate(R.layout.itemlist_deck), itemClick, itemLongClick)
+        }
+
+        override fun onBindContentHolder(model: FirebaseParsers.DeckParser, viewHolder: DecksAllViewHolder) {
+            viewHolder.bind(model.toDeck("", true), privateInteractor)
+        }
+
+        override fun onSyncEnd() {
+            decks_refresh_layout.isRefreshing = false
+        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -38,19 +48,19 @@ class DecksOwnerFragment : DecksPublicFragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        decks_recycler_view.adapter = firebaseDecksAdapter
+        decks_recycler_view.adapter = ownerDecksAdapter
         decks_recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
                 if (dy < 0) {
                     return
                 }
                 val layoutManager = recyclerView?.layoutManager as LinearLayoutManager
-                if (layoutManager.findLastVisibleItemPosition() >= firebaseDecksAdapter.getContentCount() - 3) {
-                    firebaseDecksAdapter.more()
+                if (layoutManager.findLastVisibleItemPosition() >= ownerDecksAdapter.getContentCount() - 3) {
+                    ownerDecksAdapter.more()
                 }
             }
         })
-        decks_refresh_layout.setOnRefreshListener { firebaseDecksAdapter.reset() }
+        decks_refresh_layout.setOnRefreshListener { ownerDecksAdapter.reset() }
         setHasOptionsMenu(true)
         configLoggedViews()
     }
@@ -69,65 +79,6 @@ class DecksOwnerFragment : DecksPublicFragment() {
             decksToShow.forEach { Timber.d("Decks: %s", it.toString()) }
             decksAdapter.showDecks(decksToShow, last)
         })
-    }
-
-    class OwnerFirebaseAdapter(val itemClick: (View, Deck) -> Unit, val itemLongClick: (View, Deck) -> Boolean,
-                               val privateInteractor: PrivateInteractor, pageSize: Int,
-                               val onSyncEnd: (() -> Unit)? = null) :
-            FirebaseRecyclerViewAdapter<FirebaseParsers.DeckParser, DecksAllViewHolder>(
-                    FirebaseParsers.DeckParser::class.java, R.layout.itemlist_deck, DecksAllViewHolder::class.java,
-                    privateInteractor.getOwnedPrivateDecksRef(), pageSize, false) {
-
-        var VIEW_TYPE_HEADER = 0
-        var VIEW_TYPE_CONTENT = 1
-        var VIEW_TYPE_LOADING = 2
-
-        private var synced: Boolean = false
-
-        override val snapShotOffset: Int = 1
-
-        fun getContentCount(): Int = super.getItemCount()
-
-        override fun getItemCount(): Int = super.getItemCount() + 2
-
-        override fun getItemViewType(position: Int): Int {
-            return when (position) {
-                0 -> VIEW_TYPE_HEADER
-                itemCount - 1 -> VIEW_TYPE_LOADING
-                else -> VIEW_TYPE_CONTENT
-            }
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DecksAllViewHolder {
-            val view = when (viewType) {
-                VIEW_TYPE_HEADER -> LinearLayout(parent.context).apply { minimumHeight = 1 }
-                VIEW_TYPE_LOADING -> parent.inflate(R.layout.itemlist_loading)
-                else -> parent.inflate(R.layout.itemlist_deck)
-            }
-            return DecksAllViewHolder(view, itemClick, itemLongClick)
-        }
-
-        override fun populateViewHolder(viewHolder: DecksAllViewHolder, model: FirebaseParsers.DeckParser?, position: Int) {
-            if (model != null) {
-                viewHolder.bind(model.toDeck("", true), privateInteractor)
-            } else {
-                Timber.d("Loading: " + synced)
-                viewHolder.itemView.loadingBar?.visibility = if (synced) View.GONE else View.VISIBLE
-            }
-        }
-
-        override fun onSyncStatusChanged(synced: Boolean) {
-            this.synced = synced
-            notifyItemChanged(itemCount - 1)
-            if (synced) {
-                onSyncEnd?.invoke()
-            }
-        }
-
-        override fun onArrayError(firebaseError: DatabaseError) {
-            Timber.d(firebaseError.toException(), firebaseError.toString())
-        }
-
     }
 
 }
