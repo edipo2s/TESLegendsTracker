8e440b44ece2e8f5c6f810bb03cca3361c0aee7d
EdipoSouza <edipo2s@gmail.com>
2017-01-03 23:38:21 -0200

    Created Matches fragment

---
+++ app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/matches/tabs/MatchesHistory.kt
@@ -0,0 +1,24 @@
+package com.ediposouza.teslesgendstracker.ui.matches.tabs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ediposouza.teslesgendstracker.R
+import com.ediposouza.teslesgendstracker.ui.base.BaseFragment
+import com.ediposouza.teslesgendstracker.util.inflate
+
+/**
+ * Created by EdipoSouza on 1/3/17.
+ */
+class MatchesHistory : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return container?.inflate(R.layout.fragment_matches_history)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
