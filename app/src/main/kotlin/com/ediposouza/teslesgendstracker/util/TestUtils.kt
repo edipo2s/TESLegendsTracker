4af0d2e484e9fc54a31c0158ef94cf0fd6be17f0
EdipoSouza <edipo2s@gmail.com>
2017-01-10 21:26:54 -0200

    Matches history section with date

--- app/src/main/kotlin/com/ediposouza/teslesgendstracker/util/TestUtils.kt
+++ app/src/main/kotlin/com/ediposouza/teslesgendstracker/util/TestUtils.kt
@@ -1,6 +1,12 @@
 package com.ediposouza.teslesgendstracker.util
 
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.ediposouza.teslesgendstracker.R
 import com.ediposouza.teslesgendstracker.data.*
+import com.ediposouza.teslesgendstracker.ui.matches.tabs.MatchesHistoryFragment
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersAdapter
+import org.threeten.bp.LocalDateTime
 
 /**
  * Created by EdipoSouza on 1/5/17.
@@ -13,33 +19,76 @@ object TestUtils {
                 MatchDeck("", Class.ASSASSIN, DeckType.AGGRO, ""),
                 MatchDeck("", Class.BATTLEMAGE, DeckType.AGGRO, ""),
                 MatchDeck("", Class.CRUSADER, DeckType.AGGRO, ""),
-                MatchDeck("", Class.MAGE, DeckType.AGGRO, ""),
-                MatchDeck("", Class.MONK, DeckType.AGGRO, ""),
-                MatchDeck("", Class.SCOUT, DeckType.AGGRO, ""),
-                MatchDeck("", Class.SORCERER, DeckType.AGGRO, ""),
-                MatchDeck("", Class.SPELLSWORD, DeckType.AGGRO, ""),
-                MatchDeck("", Class.WARRIOR, DeckType.AGGRO, ""),
-                MatchDeck("", Class.STRENGTH, DeckType.AGGRO, ""),
-                MatchDeck("", Class.INTELLIGENCE, DeckType.AGGRO, ""),
-                MatchDeck("", Class.AGILITY, DeckType.AGGRO, ""),
-                MatchDeck("", Class.WILLPOWER, DeckType.AGGRO, ""),
-                MatchDeck("", Class.ENDURANCE, DeckType.AGGRO, ""),
-                MatchDeck("", Class.NEUTRAL, DeckType.AGGRO, "")
+                MatchDeck("", Class.MAGE, DeckType.COMBO, ""),
+                MatchDeck("", Class.MONK, DeckType.COMBO, ""),
+                MatchDeck("", Class.SCOUT, DeckType.COMBO, ""),
+                MatchDeck("", Class.SORCERER, DeckType.COMBO, ""),
+                MatchDeck("", Class.SPELLSWORD, DeckType.CONTROL, ""),
+                MatchDeck("", Class.WARRIOR, DeckType.CONTROL, ""),
+                MatchDeck("", Class.STRENGTH, DeckType.CONTROL, ""),
+                MatchDeck("", Class.INTELLIGENCE, DeckType.CONTROL, ""),
+                MatchDeck("", Class.AGILITY, DeckType.MIDRANGE, ""),
+                MatchDeck("", Class.WILLPOWER, DeckType.MIDRANGE, ""),
+                MatchDeck("", Class.ENDURANCE, DeckType.MIDRANGE, ""),
+                MatchDeck("", Class.NEUTRAL, DeckType.MIDRANGE, "")
         )
         return mutableListOf<Match>().apply {
             for ((indexPlayer, playerDeck) in decks.withIndex()) {
                 for ((indexOpponent, opponentDeck) in decks.withIndex()) {
                     addAll(mutableListOf<Match>().apply {
                         for (i in 1..indexPlayer + 1) {
-                            add(Match("", false, playerDeck, opponentDeck, MatchMode.RANKED, "2016_12", 0, false, true))
+                            val uuid = LocalDateTime.now().minusDays(playerDeck.type.ordinal.toLong()).toString()
+                            add(Match(uuid, false, playerDeck, opponentDeck, MatchMode.RANKED, "2016_12", 0, false, true))
                         }
                         for (i in 1..indexOpponent + 1) {
-                            add(Match("", false, playerDeck, opponentDeck, MatchMode.RANKED, "2016_12", 0, false, false))
+                            val uuid = LocalDateTime.now().minusDays(opponentDeck.type.ordinal.toLong()).toString()
+                            add(Match(uuid, false, playerDeck, opponentDeck, MatchMode.RANKED, "2016_12", 0, false, false))
                         }
                     })
                 }
             }
         }
     }
+
+    fun getTestMatchesHistoryAdapter() = TestMatchesHistoryAdapter()
+
+    class TestMatchesHistoryAdapter : RecyclerView.Adapter<MatchesHistoryFragment.MatchViewHolder>(),
+            StickyRecyclerHeadersAdapter<MatchesHistoryFragment.MatchViewHolder> {
+
+        val items = TestUtils.getTestMatches()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchesHistoryFragment.MatchViewHolder {
+            return MatchesHistoryFragment.MatchViewHolder(parent.inflate(R.layout.itemlist_match_history))
+        }
+
+
+        override fun onBindViewHolder(holder: MatchesHistoryFragment.MatchViewHolder?, position: Int) {
+            holder?.bind(items[position])
+        }
+
+        override fun getItemCount(): Int = items.size
+
+        override fun onCreateHeaderViewHolder(parent: ViewGroup): MatchesHistoryFragment.MatchViewHolder {
+            return MatchesHistoryFragment.MatchViewHolder(parent.inflate(R.layout.itemlist_match_history_section))
+        }
+
+        override fun onBindHeaderViewHolder(holder: MatchesHistoryFragment.MatchViewHolder?, position: Int) {
+            holder?.bindSection(LocalDateTime.parse(items[position].uuid).toLocalDate())
+        }
+
+        override fun getHeaderId(position: Int): Long {
+            val date = LocalDateTime.parse(items[position].uuid).toLocalDate()
+            return date.year + date.monthValue + date.dayOfMonth.toLong()
+        }
+
+
+        fun getContentCount(): Int = items.size
+        fun more() {}
+        fun reset() {
+            notifyDataSetChanged()
+        }
+
+    }
+
 
 }
