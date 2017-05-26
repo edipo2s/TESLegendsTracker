f0c0328353d0fa9be6a36d24fffe6ed02a5b26f3
EdipoSouza <edipo2s@gmail.com>
2017-05-26 15:00:23 -0300

    Update about screen and reward card info

--- app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/cards/CardActivity.kt
+++ app/src/main/kotlin/com/ediposouza/teslesgendstracker/ui/cards/CardActivity.kt
@@ -286,7 +286,7 @@ class CardActivity : BaseActivity() {
         if (card.season.isNotEmpty()) {
             val yearMonth = YearMonth.parse(card.season, DateTimeFormatter.ofPattern(SEASON_UUID_PATTERN))
             val month = yearMonth.month.getDisplayName(TextStyle.FULL, Locale.getDefault())
-            card_reward.text = "$month/${yearMonth.year} ${getString(R.string.season_reward)}"
+            card_reward.text = "$month/${yearMonth.year}"
             card_reward_label.visibility = View.VISIBLE
         }
         card_race.text = card.race.name.toLowerCase().capitalize().replace("_", " ")
